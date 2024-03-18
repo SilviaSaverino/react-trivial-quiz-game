361bf4dda7cda697a76ef25f639c879db7fb71f6
Silvia Saverino <silviasave89@gmail.com>
2024-03-18 17:50:05 +0000

    get answers into an array and add a shuffle function . Style selected answer

--- src/Quiz.jsx
+++ src/Quiz.jsx
@@ -1,16 +1,42 @@
-import React from "react"
+import React, { useState, useEffect } from "react";
 
 export default function Quiz(props) {
+    const { question, correctAnswer, incorrectAnswers } = props;
+    const [shuffledAnswers, setShuffledAnswers] = useState([]);
+    const [selectedAnswer, setSelectedAnswer] = useState()
 
-    const { question, correctAnswer, incorrectAnswers } = props
+    useEffect(() => {
+        const allAnswers = [...incorrectAnswers, correctAnswer];
+        console.log(allAnswers)
+        setShuffledAnswers(shuffle(allAnswers));
+    }, [correctAnswer, incorrectAnswers]);
+
+    function shuffle(array) {
+        for (let i = array.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+        return array;
+    }
+  
+    function setClickedAnswerStyle(answer){
+        setSelectedAnswer(answer)
+    }
+    const selectedAnswerStyle = {
+        backgroundColor:  "#D6DBF5"
+    };
+
     return (
         <div className="quiz-container">
             <h3 className="questions">{question}</h3>
             <div className="answer-container">
-            <button className="correct-answer answer-btn">{correctAnswer}</button>
-
-                {incorrectAnswers.map((wrongAnswer, index) => (
-                     <button className="wrong-answer answer-btn" key={index}>{wrongAnswer}</button>
+            {shuffledAnswers.map((answer, index) => (
+                    <button 
+                        onClick={() => setClickedAnswerStyle(answer)} 
+                        key={index} 
+                        className="answer-btn"
+                        style={selectedAnswer === answer ? selectedAnswerStyle : {}}
+                        >{answer}</button>
                 ))}
             </div>
         </div>
