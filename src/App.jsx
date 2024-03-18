6e1f5d3ac71ea4af0462484be0e624a6dc016b61
Silvia Saverino <silviasave89@gmail.com>
2024-03-18 17:24:30 +0000

    conditionally render content on the page, and conditionally making api call

--- src/App.jsx
+++ src/App.jsx
@@ -4,29 +4,42 @@ import Quiz from './Quiz';
 
 function App() {
 
-  const [questions, setQuestions] = useState()
+  const [startQuiz, setStartQuiz] = useState(false)
+  const [questions, setQuestions] = useState([])
 
   useEffect(() => {
-    fetch("https://opentdb.com/api.php?amount=6&difficulty=medium&type=multiple")
-      .then(res => res.json())
-      .then(data => {setQuestions(data.results)
-      console.log(data);})
-  }, []);
+    if (startQuiz) {
+      fetch("https://opentdb.com/api.php?amount=6&difficulty=medium&type=multiple")
+        .then(res => res.json())
+        .then(data => {
+          setQuestions(data.results);
+          console.log(data);
+        });
+    }
+  }, [startQuiz]);
+
+  function toggleStartState() {
+    setStartQuiz(prevStart => !prevStart);
+  }
 
   return (
     <main>
       <img className="blue-blob" src="./src/assets/images/blue-blob.png" alt="blue blob" />
       <img className="yellow-blob" src="./src/assets/images/yellow-blob.png" alt="yellow blob" />
+      {!startQuiz && 
       <div className="initial-page">
         <h2 className="title">Trivial Quiz</h2>
         <p className='content'>Test your knowledge</p>
-        <button className='start-quiz-btn'>Start the quiz!</button>
-      </div>
-      <div>
-      {questions && questions.map((quiz, index) => (
-          <Quiz key={index} question={quiz.question} correctAnswer={quiz.correct_answer} />
-        ))}
-      </div>
+        <button onClick={toggleStartState}className='start-quiz-btn'>Start the quiz!</button>
+      </div> 
+      }
+      {startQuiz && (
+        <div>
+          {questions.map((quiz, index) => (
+            <Quiz key={index} question={quiz.question} correctAnswer={quiz.correct_answer} />
+          ))}
+        </div>
+      )}
     </main>
   );
 }
